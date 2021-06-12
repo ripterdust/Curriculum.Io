c66da3a005b1a8ba113627f792b4d692623e512e
Bryan <bryantello2010@hotmail.com>
2021-06-12 11:37:53 -0600

    Agregadas las categorías

--- src/components/Template.js
+++ src/components/Template.js
@@ -8,11 +8,11 @@ export const Template = () => {
 
             <div className="templatesList">
                 <div className="cats">
-                    <Link><i class="lni lni-gallery"></i>All Templates</Link>
-                    <Link><i className="lni lni-star"></i>Simple</Link>
-                    <Link><i className="lni lni-brush-alt"></i>Creative</Link>
-                    <Link><i className="lni lni-briefcase"></i>Professional</Link>
-                    <Link><i className="lni lni-rocket"></i>Modern</Link>
+                    <Link to='/templates/' ><i class="lni lni-gallery"></i>All Templates</Link>
+                    <Link to='/templates/simple' ><i className="lni lni-star"></i>Simple</Link>
+                    <Link to='/templates/creative' ><i className="lni lni-brush-alt"></i>Creative</Link>
+                    <Link to='/templates/pro' ><i className="lni lni-briefcase"></i>Professional</Link>
+                    <Link to='/templates/modern' ><i className="lni lni-rocket"></i>Modern</Link>
                 </div>
             </div>
         </div>
