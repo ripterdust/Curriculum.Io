1607bd691a61740a63d30241268173c0d0770db2
Bryan <bryantello2010@hotmail.com>
2021-06-12 02:23:39 -0600

    Agregados los íconos y la ruta opcional en react router

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Index}  />
-        <Route path='/templates' component={Template} />
+        <Route path='/templates/:category?' component={Template} />
 
         <Route component={Error404}  />
       </Switch>
--- src/components/Template.js
+++ src/components/Template.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Template = () => {
     return (
         <div className="templatePage" >
             Templates page
+
+            <div className="templatesList">
+                <div className="cats">
+                    <Link><i class="lni lni-gallery"></i>All Templates</Link>
+                    <Link><i className="lni lni-star"></i>Simple</Link>
+                    <Link><i className="lni lni-brush-alt"></i>Creative</Link>
+                    <Link><i className="lni lni-briefcase"></i>Professional</Link>
+                    <Link><i className="lni lni-rocket"></i>Modern</Link>
+                </div>
+            </div>
         </div>
     )
 }
