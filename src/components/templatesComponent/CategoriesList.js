dde54e787b03297d1502d020013dbe60cf6a38a1
Bryan <bryantello2010@hotmail.com>
2021-06-12 13:49:09 -0600

    Agregado el archivo CategoriesList.js

---
+++ src/components/templatesComponent/CategoriesList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const CategoriesList = ({ category }) => {
+    return (
+        <div className='listTemplates'>
+            <div className="cards">
+                Acá va la lista de las plantillas
+            </div>
+        </div>
+    )
+}
