f4a5d4c8dc55852f1f96ed43466d57652d5e0ae0
Bryan <bryantello2010@hotmail.com>
2021-06-12 13:32:05 -0600

    Terminado el footer

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -3,7 +3,18 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div className="footer">
-            Acá irá el Footer
+            <div className="social">
+                Connect with my social media.
+                <div className="links">
+                    <a href=""><i class="lni lni-facebook-oval"></i></a>
+                    <a href=""><i class="lni lni-linkedin-original"></i></a>
+                    <a href=""><i class="lni lni-whatsapp"></i></a>
+                </div>
+            </div>
+
+            <div className="copy">
+                &copy; Copyright 2021 - Bryan Guillermo Arévalo
+            </div>
         </div>
     )
 }
