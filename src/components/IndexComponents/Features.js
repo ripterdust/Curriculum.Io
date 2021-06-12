c3819992760bd32c756a2f115d9c6e59bee7f6b6
Bryan <bryantello2010@hotmail.com>
2021-06-12 01:12:20 -0600

    Agregado el componente "Features" de la página de inicio en donde se mostrará n las opciones que se pueden utilizar

---
+++ src/components/IndexComponents/Features.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Features = () => {
+    return (
+        <div className="features">
+            <div className="title">
+                Effortlessly make a job-worthy resume and cover letter that gets you hired faster
+            </div>
+            <div className="demo">
+
+            </div>
+        </div>
+    )
+}
