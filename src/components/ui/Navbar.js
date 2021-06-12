53a30d3e12a0dddfc746f06783213d110394c993
Bryan <bryantello2010@hotmail.com>
2021-06-12 00:26:42 -0600

    Terminada la barra de navegación

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import { NavbarOptions } from './NavbarOptions';
+
+
 
 // This component ins the navigation bar
 export const Navbar = () => {
     return <nav>
-        <div className="logo">
-            <NavLink exact to='/'><i className="lni lni-wallet"></i>Curriculum.io</NavLink>
+        <NavLink exact to='/' className="mainLink">
+            <i className="wallet lni lni-wallet"></i>
+        </NavLink>
+        <div className="content">
+            <NavLink exact to='/' className="home" activeClassName="navActive">Home</NavLink>
+            {/* Dropdown */}
+            <div className="dropdown">
+                <NavLink exact to='/templates' className="menu" activeClassName="navActive">Curriculum</NavLink>
+                <div className="dropdown-content">
+                    <NavbarOptions />
+                </div>
+            </div>
         </div>
-        <NavLink to='/create' />
     </nav>
 }
---
+++ src/components/ui/NavbarOptions.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+export const NavbarOptions = () => {
+    return (
+        <>
+        <div className="left">
+            <div className="title">
+                <Link to='/templates'>
+                    Curriculum templates
+                </Link>
+                <Link to='/templates' className="all">
+                    View all
+                </Link>
+            
+            </div>
+
+            <div className="categories">
+                <Link className="cat" to='/templates/simple'>
+                    <div className="title"><i class="lni lni-star"></i>Simple</div>
+                    <div className="description">
+                        Clean, straightforward templates aiming to keep the focus on your letter’s writing and content
+                    </div>
+                </Link>
+                <Link className="cat" to='/templates/pro'>
+                    <div className="title"><i class="lni lni-briefcase"></i>Profesional</div>
+                    <div className="description">
+                    Polished designs to help you highlight your knowledge and expertise in formal fields
+                    </div>
+                </Link>
+                <Link className="cat" to="/templates/modern" >
+                    <div className="title"><i class="lni lni-rocket"></i>Modern</div>
+                    <div className="description">
+                        Sleek and attractive designs for forward-thinking candidates in innovative fields
+                    </div>
+                </Link>
+                <Link className="cat" to="/templates/creative" >
+                    <div className="title"><i class="lni lni-brush-alt"></i>Creative</div>
+                    <div className="description">
+                        Stylish templates defined by a unique approach to show off your originality and inspiration
+                    </div>
+                </Link>
+            </div>
+
+        </div>
+        <div className="right">
+            <img src="/img/nav.png" alt="" />
+        </div>
+        </>
+    )
+}
