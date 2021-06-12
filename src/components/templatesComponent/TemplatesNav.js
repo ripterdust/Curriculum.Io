76e1df92e0917ff4478583682c5d1277204000ba
Bryan <bryantello2010@hotmail.com>
2021-06-12 13:17:43 -0600

    Terminada la barra de selección de plantillas

--- src/components/templatesComponent/TemplatesNav.js
+++ src/components/templatesComponent/TemplatesNav.js
@@ -3,12 +3,32 @@ import { NavLink } from 'react-router-dom'
 export const TemplatesNav = () => {
     return (
         <div className="templatesNav">
-            <div className="selection">
-                <NavLink to="/templates/" activeClassName="categoryActive" >All</NavLink>
-                <NavLink to="/templates/simple" activeClassName="categoryActive" >Simple</NavLink>
-                <NavLink to="/templates/modern" activeClassName="categoryActive" >Modern</NavLink>
-                <NavLink to="/templates/professional" activeClassName="categoryActive" >Professional</NavLink>
-                <NavLink to="/templates/creative" activeClassName="categoryActive" >Creative</NavLink>
+            <div className="elements">
+                <NavLink exact to="/templates/" 
+                activeClassName="categoryActive" 
+                className="element">
+                    <i class="lni lni-list"></i>All
+                </NavLink>
+                <NavLink to="/templates/simple" 
+                activeClassName="categoryActive" 
+                className="element">
+                    <i className="lni lni-star"></i>Simple
+                </NavLink>
+                <NavLink to="/templates/modern" 
+                activeClassName="categoryActive" 
+                className="element">
+                    <i className="lni lni-rocket"></i>Modern
+                </NavLink>
+                <NavLink to="/templates/professional" 
+                activeClassName="categoryActive" 
+                className="element">
+                    <i className="lni lni-briefcase"></i>Professional
+                </NavLink>
+                <NavLink to="/templates/creative" 
+                activeClassName="categoryActive" 
+                className="element">
+                    <i className="lni lni-brush-alt"></i>Creative
+                </NavLink>
             </div>
         </div>
     )
