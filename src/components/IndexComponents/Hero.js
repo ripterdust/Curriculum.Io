47b0faf22564d20126cf54f2ff592e65c52605b5
Bryan <bryantello2010@hotmail.com>
2021-06-12 02:05:26 -0600

    Intenté agregar una animación paralax, pero no se veía bien

--- src/components/IndexComponents/Hero.js
+++ src/components/IndexComponents/Hero.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const Hero = () => {
+         
     return (
         <div className="hero">
-            <div className="text">
+            <div className="text parallax">
                 <div className="title">
                     Only 2% of resumes make it past the first round. Be in the top 2%
                 </div>
-                <div className="subTitle">
+                <div className="subTitle parallax ">
                     Use professional field-tested resume templates that follow the exact ‘resume rules’ employers look for. Easy to use and done within minutes - try now for free! 
                 </div>
-                <div className="btn">
+                <div className="btn parallax">
                     <Link to='/templates' className='button' >Create my resume</Link>
                 </div>
             </div>
