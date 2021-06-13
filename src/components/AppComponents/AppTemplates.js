465665cacfecc85d2415dce62715c1081f571408
Bryan <bryantello2010@hotmail.com>
2021-06-12 21:15:41 -0600

    Agregados los componentes para hacer el slider

--- src/components/AppComponents/AppTemplates.js
+++ src/components/AppComponents/AppTemplates.js
@@ -3,13 +3,15 @@ import { data } from '../../data/data';
 import { useFilter } from '../../hooks/useFilter';
 import { ResumeItem } from './ResumeItem';
 
+
+
 export const AppTemplates = () => {
+
 
     const [list] = useFilter({data})
 
-    return (
-        <div className="SlideShow">
-            {list.map((el, i) => <div className="card" key={i} ><ResumeItem data={el} /></div> )}
-        </div>
-    )
+
+    return <div className="carousel">
+        
+    </div>
 }
