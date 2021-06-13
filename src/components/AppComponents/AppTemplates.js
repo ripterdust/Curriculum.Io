95ce6f0e38acc7a0d90be7cf6a684ae05abb3542
Bryan <bryantello2010@hotmail.com>
2021-06-12 21:35:29 -0600

    Agregados los botones del slide

--- src/components/AppComponents/AppTemplates.js
+++ src/components/AppComponents/AppTemplates.js
@@ -10,8 +10,17 @@ export const AppTemplates = () => {
 
     const [list] = useFilter({data})
 
+    let slideIndex = 1;
 
     return <div className="carousel">
-        
+        {
+            list.map((el, i) => <div className="card" key={i} >
+                <ResumeItem data={el} />
+            </div> )
+        }
+
+        <button className="prev">&#10094;</button>
+        <button className="next" >&#10095;</button>
+
     </div>
 }
