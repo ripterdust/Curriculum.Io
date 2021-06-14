2916d42bc817455b383cfd54279a7c2d997858ad
Bryan <bryantello2010@hotmail.com>
2021-06-14 12:17:50 -0600

    Arreglado el formulario, sólo faltan los estilos

--- src/components/AppComponents/CreateForm.js
+++ src/components/AppComponents/CreateForm.js
@@ -6,9 +6,8 @@ export const CreateForm = () => {
     // React-router-dom hooks
     const {id} = useParams();
     const history = useHistory();
-
     // UseState
-    const [data, setData] = useState({id});
+    const [data, setData] = useState({id: id});
 
     // Change state 
     const handleInputs = (e) => {
@@ -18,7 +17,6 @@ export const CreateForm = () => {
     // Send data
     const handleSumbit = (e) => {
         e.preventDefault();
-        console.log(data)
         // Destructuriying state
         let { id, name, profession} = data;
 
@@ -28,39 +26,70 @@ export const CreateForm = () => {
     }
 
     return (<form onSubmit={handleSumbit} onChange={handleInputs} action='/'>
-            <div className="personalData">
-                {/* Name information */}
-                <input 
-                type="text" 
-                name='name' 
-                onChange={handleInputs} 
-                placeholder="Full name" required />
-                {/* Profession */}
-                <input type="text" 
-                name='profession' 
-                onChange={handleInputs} 
-                placeholder="Profession" required />
+            <div className="inputs">
+                <div className="personalData">
+                    <span>Personal data</span>
+                    {/* Name information */}
+                    <input 
+                    type="text" 
+                    name='name' 
+                    onChange={handleInputs} 
+                    placeholder="Full name" required />
+                    {/* Profession */}
+                    <input type="text" 
+                    name='profession' 
+                    onChange={handleInputs} 
+                    placeholder="Profession" required />
 
-                {/* Contact Data */}
-                <input 
-                type="email"  
-                name='email'
-                placeholder="E-mail"
-                required/>
+                    {/* Contact Data */}
+                    <input 
+                    type="email"  
+                    name='email'
+                    placeholder="E-mail"
+                    required/>
 
-                {/* Phone */}
-                <input 
-                type="text" 
-                name='phone' 
-                placeholder="Phone"
-                required/>
+                    {/* Phone */}
+                    <input 
+                    type="text" 
+                    name='phone' 
+                    placeholder="Phone"
+                    required/>
 
-            </div>
-            <div className="workInfo">
+                </div>
+                <div className="workInfo">
+                    <span>Laboral data</span>
+                    <input type="text" 
+                    name='Enterprise' 
+                    placeholder="Enterprise" 
+                    required/>
+                    <input type="text" 
+                    name='jobTitle'
+                    placeholder="Job Title"
+                    required
+                    />
+                </div>
 
-            </div>
-
-            <div className="scolarship">
+                <div className="scolarship">
+                <span>Scolarship</span>
+                |<input type="text" 
+                    name='university' 
+                    placeholder="University name" 
+                    required/>
+                    <input type="text" 
+                    name='jobTitle'
+                    placeholder="Job Title"
+                    required
+                    />
+                    <input type="text" 
+                    name='grade' 
+                    placeholder="Grade" 
+                    required/>
+                    <input type="text" 
+                    name='jobTitle'
+                    placeholder="Job Title"
+                    required
+                    />
+                </div>
 
             </div>
 
